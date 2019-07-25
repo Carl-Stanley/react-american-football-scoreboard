8fe87073c0cc9562c45388e2e885eac924f577f4
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-07-24 19:40:18 -0700

    Commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   
-  let [homeScore,newHomeScore] = useState(32);
-  let [awayScore,newAwayScore] = useState(32);
+  let [homeScore,newHomeScore] = useState(0);
+  let [awayScore,newAwayScore] = useState(0);
   
   let homeName = 'Lions';
   let awayName = "Tigers";
